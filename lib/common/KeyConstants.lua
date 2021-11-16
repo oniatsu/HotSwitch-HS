57551b11c3fdf2302fedb3dae78067f949c6fdcb
oniatsu <5919569+oniatsu@users.noreply.github.com>
2021-11-16 19:48:42 +0900

    Remove a invalid key

--- lib/common/KeyConstants.lua
+++ lib/common/KeyConstants.lua
@@ -3,7 +3,7 @@ local KeyConstants = {
     -- Sometimes, some special keys don't work
     ALL_KEYS = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m",
                 "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",
-                "-", "[", "]", ",", ".", "/"},
+                "-", "[", "]", ".", "/"},
 
     SHIFTABLE_KEYS = {"q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"},
 
